8d98d780b5434e5f6ee8a1419761b2f98f97d8ce
Gogik <gog.ortey@yandex.ru>
2025-03-03 02:40:45 +0500

    15 Final

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
+// Обработчик нажатия на кнопку, которая показывает окно
+document.getElementById('open-window').addEventListener('click', function() {
+    this.style.display = 'none';
+    document.querySelector('.main-window').style.display = 'grid';
+    centerElement(draggableElement);
+});
 
-// Делает элемент перемещаемым по странице, при помощи мыши
+// Обработчик нажатия на кнопку закрытия окна
+document.getElementById('closed-window').addEventListener('click', function() {
+    document.querySelector('.main-window').style.display = 'none';
+    document.getElementById('open-window').style.display = 'block';
+});
+
+
+// Делает окно перемещаемым по странице, при помощи мыши
 const dragElement = (element, dragArea) => {
     let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
 
@@ -29,6 +42,7 @@ const dragElement = (element, dragArea) => {
     dragArea.onmousedown = dragMouseDown;
 };
 
+// Центрирует окно
 const centerElement = (element) => {
     const windowHeight = window.innerHeight;
     const windowWidth = window.innerWidth;
