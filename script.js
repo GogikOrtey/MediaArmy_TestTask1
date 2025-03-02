d42f58901cbb9edbe3c91b46bb5497d4aba2fe18
Gogik <gog.ortey@yandex.ru>
2025-03-03 01:14:15 +0500

    5 Теперь окно может перемещаться по странице

---
+++ script.js
@@ -0,0 +1,33 @@
+
+// Делает элемент перемещаемым по странице, при помощи мыши
+const dragElement = (element) => {
+    let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+
+    const dragMouseDown = (e) => {
+        e.preventDefault();
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        document.onmouseup = closeDragElement;
+        document.onmousemove = elementDrag;
+    };
+
+    const elementDrag = (e) => {
+        e.preventDefault();
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        element.style.top = (element.offsetTop - pos2) + "px";
+        element.style.left = (element.offsetLeft - pos1) + "px";
+    };
+
+    const closeDragElement = () => {
+        document.onmouseup = null;
+        document.onmousemove = null;
+    };
+
+    element.onmousedown = dragMouseDown;
+};
+
+const draggableElement = document.getElementById("draggable");
+dragElement(draggableElement);
